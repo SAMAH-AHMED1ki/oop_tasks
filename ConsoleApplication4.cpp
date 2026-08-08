27d46e1641d90574b160e33fcb0063c2bfd61386
SAMAH-AHMED1ki <sammahmedzz50@gmail.com>
2026-08-08 14:29:06 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,20 @@
+
+#include <vector>
+using namespace std;
+#include <iostream>
+ 
+struct bankcustomer{
+	string name;
+	string address;
+	string mobile;
+	string birth_of_date;
+	double balance;
+	string  email;
+	bool is_active;
+};
+;
+
+int main()
+{
+	bankcustomer c1;
+}
