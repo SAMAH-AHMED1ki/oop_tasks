d6f9b17f5b50d5a034e8413c474992bb8cbeba2a
SAMAH-AHMED1ki <sammahmedzz50@gmail.com>
2026-08-08 16:17:20 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,16 @@
+
+#include <vector>
+#include <iostream>
+using namespace std;
+template <typename T, int N>
+struct queue_task {
+	T arr[N];
+
+};
+
+int main()
+{
+	queue_task <int, 100>r;
+	
+
+}
